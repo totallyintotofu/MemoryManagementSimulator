172303b2e71bf47f1aa662f450f5e8be6d74c7c5
briarHarrison <harri25j@mtholyoke.edu>
2017-11-18 17:59:42 -0500

    More setup

--- MemoryManager.c
+++ MemoryManager.c
@@ -1,16 +1,16 @@
-//MemoryManager simulates the way that an operating system might handle a process' memory heap. 
-//It can allocate and free memory. It cannot currently support defragmentation.
-
 //Lisa Chen and Briar Harrison
 //MHC CS 322 - Operating Systems
 //2017
+
+//MemoryManager simulates the way that an operating system might handle a process' memory heap. 
+//It can allocate and free memory. It cannot currently support defragmentation.
 
 #include <unistd.h>
 #include <stdio.h>
 
 //main is just for testing purposes
 int main (int argc, char **argv){
-	printf("%d\n", getpagesize());
+	//printf("%d\n", getpagesize());
 
 	return 0;
 }
