9a1b2c7b65078b5b58386e6c3882c3eb8fac0591
briarHarrison <harri25j@mtholyoke.edu>
2017-11-18 21:51:49 -0500

    Beginning Mem_Init
    Errors exist. I am getting a Operation Not Supported error at runtime.
    I believe it has to do with mmap

--- MemoryManager.c
+++ MemoryManager.c
@@ -7,10 +7,38 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include "mem.h"
+#include <fcntl.h>
+
+//Mem_Init 
+void *Mem_Init (int sizeOfRegion){
+	printf("page size: %d\n", getpagesize());
+	int numPages = ceil((double) sizeOfRegion / (double) getpagesize());
+	printf("numPages: %d\n", numPages);
+	int requestSize = numPages * getpagesize();
+	printf("requestSize: %d\n", requestSize);
+
+	// open the /dev/zero device
+	int fd = open("/dev/zero", O_RDWR);
+
+	// requestSize (in bytes) needs to be evenly divisible by the page size
+	void *ptr = mmap(NULL, requestSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	if (ptr == MAP_FAILED) {
+		perror("mmap"); exit(1);
+	}
+	// close the device (don't worry, mapping should be unaffected)
+	close(fd);
+
+	return ptr;
+}
 
 //main is just for testing purposes
 int main (int argc, char **argv){
-	//printf("%d\n", getpagesize());
+	printf("%p\n", Mem_Init(2000));
 
 	return 0;
 }
